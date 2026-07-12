d6434bf0bfb59294ffb214e1fbbfd5bce7f79d8d
SaiShruthiReddyD <saishruthidhesham@gmail.com>
2026-07-12 20:56:41 +0530

    Add files via upload

---
+++ SecondLarge.java
@@ -0,0 +1,28 @@
+//Second Largest element in an array
+import java.util.*;
+public class SecondLarge {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] arr=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+               int max1;
+        int max2;
+        if(arr[0]>arr[1]){
+            max1=arr[0];
+            max2=arr[1];
+        }
+        else{
+            max1=arr[1];
+            max2=arr[0];
+        }
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]>max1){
+                max2=max1;
+                max1=arr[i];
+            }
+        }System.out.println("Second largest element is:"+max2);
+    }
+}
